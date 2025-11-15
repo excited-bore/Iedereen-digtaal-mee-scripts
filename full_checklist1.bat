a74a3c6bdaba3f8b4a3dcbfbf94f95b755ca9333
excited-bore <serve-churn-spiral@duck.com>
2025-11-15 02:54:43 +0100

    Small rework in full_checklist1.bat

--- full_checklist1.bat
+++ full_checklist1.bat
@@ -5,15 +5,16 @@ pause
 
 
 REM Optional Updates
+call "%~dp0scripts\enable_optional_updates.bat"
+
 REM Windows Update
 call "%~dp0scripts\windows_update.bat"
-call "%~dp0scripts\enable_optional_updates.bat"
 
 REM Turn off password for user
 call "%~dp0scripts\no_password.bat"
 
 REM Check if winget is installed. If not, try and fix
-call "%~dp0scripts\check_winget.bat"
+powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\check_winget.ps1"
 
 REM Try and install updaters and support assisters
 powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\update-drivers.ps1"
